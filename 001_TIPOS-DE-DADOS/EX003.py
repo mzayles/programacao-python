0414a4aac87368a03c7be74f16be1b8242f86602
Mariana Alves <102690791+mzayles@users.noreply.github.com>
2025-03-30 17:28:49 -0300

    delete

--- 001_TIPOS-DE-DADOS/EX003.py
+++
@@ -1,6 +0,0 @@
-# DESAFIO 003
-
-""" Crie um programa que leia um número inteiro e imprima seu sucessor e seu antecessor """
-
-numero_inteiro = int(input("Digite um número: "))
-print(f"O antecessor de \033[1;34m{numero_inteiro}\033[0m é \033[1;34m{numero_inteiro - 1}\033[0m e seu sucessor é \033[1;34m{numero_inteiro + 1}\033[0m!")
